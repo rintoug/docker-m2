90e8134ad4028d32d9e040b06c2d07486b250ddb
Rinto George <mail.rinto@gmail.com>
2021-03-14 16:17:42 +0400

    Corrected DB name

--- .docker/apache_php/run.sh
+++ .docker/apache_php/run.sh
@@ -16,6 +16,6 @@ tar -xvf magento-ce-2.4.1-2020-09-25-04-17-23.tar.gz -C app
 rm magento-ce-2.4.1-2020-09-25-04-17-23.tar.gz
 
 
-docker-compose exec web php bin/magento setup:install --base-url="http://localhost:8091/" --db-host="db" --db-name="magneto2" --db-user="root" --db-password="test123@123" --admin-firstname="admin" --admin-lastname="admin" --admin-email="admin@admin.com" --admin-user="admin" --admin-password="admin@123" --language="en_US" --currency="USD" --timezone="America/Chicago" --use-rewrites="1" --backend-frontname="admin" --elasticsearch-host="es01" --elasticsearch-port="9200"
+docker-compose exec web php bin/magento setup:install --base-url="http://localhost:8091/" --db-host="db" --db-name="magento2" --db-user="root" --db-password="test123@123" --admin-firstname="admin" --admin-lastname="admin" --admin-email="admin@admin.com" --admin-user="admin" --admin-password="admin@123" --language="en_US" --currency="USD" --timezone="America/Chicago" --use-rewrites="1" --backend-frontname="admin" --elasticsearch-host="es01" --elasticsearch-port="9200"
 docker-compose exec web php bin/magento setup:di:compile
 docker-compose exec web php bin/magento module:disable Magento_TwoFactorAuth
