f6c936e12a560dd62808011a2fb5c70b86fa26bb
Rinto George <mail.rinto@gmail.com>
2021-03-14 17:46:05 +0400

    Corrected redis image

---
+++ env.php
@@ -0,0 +1,83 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '1f716e5b9f02c097f7c795ebb35b3524'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magneto',
+                'username' => 'root',
+                'password' => 'test123@123',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Sun, 14 Mar 2021 11:52:32 +0000'
+    ]
+];
--- run.sh
+++ run.sh
@@ -11,3 +11,5 @@ rm magento-ce-2.4.1-2020-09-25-04-17-23.tar.gz
 docker-compose exec web php bin/magento setup:install --base-url="http://localhost:8091/" --db-host="db" --db-name="magento2" --db-user="root" --db-password="test123@123" --admin-firstname="admin" --admin-lastname="admin" --admin-email="admin@admin.com" --admin-user="admin" --admin-password="admin@123" --language="en_US" --currency="USD" --timezone="America/Chicago" --use-rewrites="1" --backend-frontname="admin" --elasticsearch-host="es01" --elasticsearch-port="9200"
 docker-compose exec web php bin/magento setup:di:compile
 docker-compose exec web php bin/magento module:disable Magento_TwoFactorAuth
+docker-compose exec web php bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=magento2_redis --page-cache-redis-db=1
+docker-compose exec web php bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=magento2_redis --cache-backend-redis-db=0
